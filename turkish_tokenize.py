2a00ff5363a629228fb97d69d596f3278237f4a0
Faidon Mitzalis <fm1710@ic.ac.uk>
2020-11-01 22:34:55 +0000

    Cleared files

--- turkish_tokenize.py
+++ turkish_tokenize.py
@@ -28,35 +28,3 @@ with open("turkish_tokenized_CASED.txt", "w") as text_file:
                     text_file.write(sentence['raw']+'\n')
                     text_file.write(' '.join(tokenizer.tokenize(sentence['raw']))+'\n')
 
-exit()
-
-with open('train.txt') as fp:
-    for cnt, line in enumerate(fp):
-        image_id = (line.split('.')[0])
-        images.append('train_image.zip@/'+ image_id +'.jpg')
-        while len(image_id)<8:
-            image_id = '0'+image_id
-        frcnns.append('train_frcnn.zip@/'+ image_id +'.json')
-
-with open('../train.en') as fp:
-    for cnt, line in enumerate(fp):
-        captions_en.append(line.strip())
-
-with open('../train.de') as fp:
-    for cnt, line in enumerate(fp):
-        captions_de.append(line.strip())      
-
-# Check lengths are the same  
-assert(len(captions_en)==len(captions_de))
-
-with open('../train.json', 'w') as outfile:
-    for cnt, (caption_en, caption_de, image) in enumerate(zip(captions_en, captions_de, images)):
-            d = {'image':image, 'caption_en':caption_en, 'caption_de':caption_de}
-            json.dump(d, outfile)
-            outfile.write('\n')
-            
-with open('../train_frcnn.json', 'w') as outfile:
-    for cnt, (caption_en, caption_de, image, frcnn) in enumerate(zip(captions_en, captions_de, images, frcnns)):
-            d = {'image':image, 'caption_en':caption_en, 'caption_de':caption_de, 'frcnn':frcnn}
-            json.dump(d, outfile)
-            outfile.write('\n')
