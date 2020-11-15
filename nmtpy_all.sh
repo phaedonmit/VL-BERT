8f6503845d9d1666d548bd58f08d4f13e00c5f76
Faidon Mitzalis <phaedonmit@yahoo.gr>
2020-11-15 22:16:23 +0000

    run nmtpy on all

---
+++ nmtpy_all.sh
@@ -0,0 +1,6 @@
+root_path="/data/faidon/VL-BERT/checkpoints/generate/single_phase_epoch2/"
+ground_path="/data/faidon/VL-BERT/data/ground_truth/"
+
+echo "***************"
+echo "1. DEIMG"
+nmtpy-coco-metrics -l de "${root_path}DEIMG.txt" -r "${ground_path}DEIMG_0.txt" "${ground_path}DEIMG_1.txt" "${ground_path}DEIMG_2.txt" "${ground_path}DEIMG_3.txt" "${ground_path}DEIMG_4.txt"
