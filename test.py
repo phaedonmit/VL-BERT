1ee10883e10a735adf4ce59fbbba4998456e160a
Faidon Mitzalis <phaedonmit@yahoo.gr>
2020-12-04 16:25:14 +0000

    added files for dummy testing

--- test.py
+++ test.py
@@ -2,16 +2,20 @@
 #     [one, two, three] = args
 #     print(len(args))
 #     print(one)
-import torch
+# import torch
+import json
+import jsonlines
 
 if __name__ == "__main__":
     # check('this', 'is', 'the')
-
-    multi = torch.zeros((5,8))
-    print(multi)
-    x = [1.,2.,3.,4.,5.]
-
-    for i in range(5):
-        mlm = torch.zeros((8,))+x[i]
-        multi[i,:] = mlm
-        print(multi)
+    ann_file = "/data/ozan/datasets/mmbert/iwslt14_en_de/test_frcnn.json"
+    simple_database = list(jsonlines.open(ann_file))
+    example = simple_database[1]
+    words = example['caption_en'].split(' ')
+    with open('/data/faidon/VL-BERT/data/testing/test_frcnn.json', 'w') as outfile:
+        for cnt,  word_en in enumerate((words)):
+            d = {'caption_en': ' '.join(words[:cnt+1]),
+                 'caption_de': example['caption_de']}
+            json.dump(d, outfile)
+            outfile.write('\n')
+    print(example)
