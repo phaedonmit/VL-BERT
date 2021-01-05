08e1e9e7d0fd047519c54e30427e9a3bbbbebbac
Faidon Mitzalis <phaedonmit@yahoo.gr>
2021-01-05 10:23:05 +0200

    fix

--- nmtpy_val_master.sh
+++ nmtpy_val_master.sh
@@ -3,5 +3,5 @@ root_path=/data/faidon/VL-BERT/checkpoints/generated/
 
 for d in checkpoints/generated/single_phase_epoch[0-1][0-9]/ ; do
     ./nmtpy_val_all_tok.sh d > "${root_path}${d}/results_val.all.tok"
-    echo "${root_path}${d}/results_val.all.tok
+    echo "${root_path}${d}/results_val.all.tok"
 done
