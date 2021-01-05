221d8552dfc22445f0ed19d798acc1b0dcd11645
Faidon Mitzalis <phaedonmit@yahoo.gr>
2021-01-05 10:13:14 +0200

    check val

--- nmtpy_val_master.sh
+++ nmtpy_val_master.sh
@@ -2,5 +2,6 @@ echo 'Start evaluation for all...'
 root_path=/data/faidon/VL-BERT/checkpoints/generated/
 
 for d in checkpoints/generated/single_phase_epoch[0-9][0-9]/ ; do
-    ./nmtpy_val_all_tok.sh d > "${root_path}${d}"
+    # ./nmtpy_val_all_tok.sh d > "${root_path}${d}"
+    echo "${root_path}${d}"
 done
