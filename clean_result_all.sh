69c2873554a21ad7ca05af9c40c84320ca6ede15
Faidon Mitzalis <phaedonmit@yahoo.gr>
2021-01-06 10:19:26 +0200

    clean all val

--- clean_result_all.sh
+++ clean_result_all.sh
@@ -1,8 +1,8 @@
 echo 'Start cleaning for all...'
 
 for d in checkpoints/generated/single_phase_epoch[0-1][0-9]/ ; do
-    root_path="/data/faidon/VL-BERT${d}/results_val.all.tok"
-    new_path="/data/faidon/VL-BERT${d}/results_clean_val.all.tok"
+    root_path="/data/faidon/VL-BERT${d}results_val.all.tok"
+    new_path="/data/faidon/VL-BERT${d}results_clean_val.all.tok"
 
     sed '/*/d' < $root_path > $new_path
     sed -i 's/|//g' $new_path
