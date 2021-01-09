b49949fbcaf578976ad3137c2bce310910ec0ed6
Faidon Mitzalis <phaedonmit@yahoo.gr>
2021-01-09 11:50:51 +0200

    nmtp for endeimg only

--- clean_result_all.sh
+++ clean_result_all.sh
@@ -1,6 +1,7 @@
 echo 'Start cleaning for all...'
+DIRECTORY=$1
 
-for d in checkpoints/generated/single_phase_epoch[0-1][0-9]/ ; do
+for d in checkpoints/generated/${DIRECTORY}_epoch[0-1][0-9]/ ; do
     root_path="/data/faidon/VL-BERT/${d}results_val.all.tok"
     new_path="/data/faidon/VL-BERT/${d}results_clean_val.all.tok"
 
