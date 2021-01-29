4c966d26afae41d408e04f5a7db73a2f51d2e665
Faidon Mitzalis <phaedonmit@yahoo.gr>
2021-01-29 16:39:15 +0200

    all val

--- nmtpy_val_master.sh
+++ nmtpy_val_master.sh
@@ -2,7 +2,7 @@ echo 'Start evaluation for all...'
 root_path=/data/faidon/VL-BERT/
 DIRECTORY=$1
 
-for d in checkpoints/generated/${DIRECTORY}_epoch[1][8-9]/ ; do
+for d in checkpoints/generated/${DIRECTORY}_epoch[0-1][0-9]/ ; do
     echo "${root_path}${d}results_val.all.tok"
     ./nmtpy_val_all_tok.sh $d > "${root_path}${d}results_val.all.tok"
 done
